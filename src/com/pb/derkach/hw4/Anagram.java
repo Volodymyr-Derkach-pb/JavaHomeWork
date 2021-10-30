452db958a4c93e3e4bd3e96f1cb6b5993e0f3ed9
Volodimir Derkach <volodymyr.derkach@privatbank.ua>
2021-10-30 21:21:08 +0300

    init commit

--- src/com/pb/derkach/hw4/Anagram.java
+++ src/com/pb/derkach/hw4/Anagram.java
@@ -8,42 +8,41 @@ public class Anagram {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("введите значение 1 : ");
+        System.out.print("введите значение 1: ");
         String first = in.nextLine().toUpperCase();
 
 
-        System.out.print("введите значение 2  : ");
+        System.out.print("введите значение 2: ");
         String second = in.nextLine().toUpperCase();
 
         String result = isAnagram(first, second);
         System.out.println(result);
     }
 
-    public static String isAnagram(String first, String second) {
-
-        // get rid of allowed characters and convert to upper case
-        String st = first.replaceAll("[., ]","").toUpperCase();
-        String nd = second.replaceAll("[., ]","").toUpperCase();
-
-        // Now get rid of all alpha characters and compare to the empty string.
-        // Only if both are equal are the strings potential anagrams.
-        // Otherwise, the illegal characters would be present.
-        if (st.replaceAll("[A-Z]","").equals("") &&
-                nd.replaceAll("[A-Z]","").equals("")) {
-
-            // this is your original code
-            char[] arraySt = st.toCharArray();
-            char[] arrayNd = nd.toCharArray();
-            Arrays.sort(arraySt);
-            Arrays.sort(arrayNd);
-
-            // don't set a value just return it's an
-            // anagram
-            if (Arrays.equals(arraySt, arrayNd)) {
-                return "Anagram.";
-            }
+    private static String isAnagram(String first, String second) {
+        if (first == null || second == null || first.equals("") || second.equals("") || !first.matches("[A-Za-z,. ]+")
+                || !second.matches("[A-Za-z,. ]+")) {
+            return "не анаграма";
         }
-        // Otherwise, it's not
-        return "No Anagram.";
+        String answer = "";
+
+        String st = first.replaceAll("[,. ]+", "");
+        String nd = second.replaceAll("[,. ]+", "");
+
+        if (st.equals("") || nd.equals("")) {
+            return "не анаграма";
+        }
+
+        char[] arraySt = st.toCharArray();
+        char[] arrayNd = nd.toCharArray();
+        Arrays.sort(arraySt);
+        Arrays.sort(arrayNd);
+
+        if (Arrays.equals(arraySt, arrayNd)) {
+            answer = "анаграма.";
+        } else {
+            answer = "не анаграма.";
+        }
+        return answer;
     }
 }
